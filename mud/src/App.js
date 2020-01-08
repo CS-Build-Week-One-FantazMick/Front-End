be73e6c309f711cbac80f85a235f665d2fd92cfe
Nick-Ballenger <nickster214@gmail.com>
2020-01-08 16:28:02 -0700

    movement setup mostly

--- mud/src/App.js
+++ mud/src/App.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import ClientLogin from "./components/clientLogin/ClientLogin"
 import ClientRegistration from "./components/clientRegistration/ClientRegistration"
+import Moving from './components/clientMovement/Moving'
+import './App.css'
 function App() {
   return (
     <div>
       <p>Hello World</p>
       <canvas
-        width={window.innerWidth}
-        height={window.innerHeight}
+        width={800}
+        height={400}
         onClick={e => {
           alert(e.screenX)
         }}
       />
+      <Moving></Moving>
       <ClientRegistration/>
+      
     </div>
   )
 }
---
+++ mud/src/actions/roomActions.js
@@ -0,0 +1,27 @@
+export const title = (data) => {
+    return {
+        type: "TITLE",
+        payload: data
+    }
+}
+
+export const description = (data) => {
+    return {
+        type: "DESCRIPTION",
+        payload: data
+    }
+}
+
+export const players = (data) => {
+    return {
+        type: "PLAYERS",
+        payload: data
+    }
+}
+
+export const error_msg = (data) => {
+    return {
+        type: "ERROR_MSG",
+        payload: data
+    }
+}
